e674c3c6de3b0521d248c9109af0dad99894f376
Shung Fei <git@shungfei.com>
2023-07-24 09:12:38 +1200

    Fix icons spawning when any object hits the bottom

--- scripts/Bottom_Wall.gd
+++ scripts/Bottom_Wall.gd
@@ -15,21 +15,22 @@ func _process(delta):
 	pass
 
 
-func _on_area_2d_body_exited(body):
-	var count = 0
-	for row in Global.iconData.size():
-		for column in Global.iconData[row].size():
-			if !Global.iconData[row][column]:
-				if count > 5:
-					pass
-				else:
-					count += 1
-					var brick = brick_scene.instantiate()
-					Global.iconData[row][column] = true
-					brick.position = Vector2(60 + 100 * column, 200 + 100 * row)
-					brick.row = row
-					brick.column = column
-					var sprite = brick.get_child(0)
-					sprite.frame = rng.randi_range(0,9)
-					%LevelOne.add_child(brick)
+func _on_area_2d_body_entered(body : Node):
+	if body.name == "Ball":
+		var count = 0
+		for row in Global.iconData.size():
+			for column in Global.iconData[row].size():
+				if !Global.iconData[row][column]:
+					if count > 5:
+						pass
+					else:
+						count += 1
+						var brick = brick_scene.instantiate()
+						Global.iconData[row][column] = true
+						brick.position = Vector2(60 + 100 * column, 200 + 100 * row)
+						brick.row = row
+						brick.column = column
+						var sprite = brick.get_child(0)
+						sprite.frame = rng.randi_range(0,9)
+						%LevelOne.add_child(brick)
 	pass # Replace with function body.
