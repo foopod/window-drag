c0bbbc593d05427d3b7e2d4121af195fa52f85b2
Shung Fei <git@shungfei.com>
2023-07-23 14:54:55 +1200

    Make icons spawn across the play area

--- level_one.gd
+++ level_one.gd
@@ -5,10 +5,10 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var rng = RandomNumberGenerator.new()
-	for n in 10:
+	for n in 19:
 		for m in 5:
 			var brick = brick_scene.instantiate()
-			brick.position = Vector2(200 + 100 * n, 300 + 100 * m)
+			brick.position = Vector2(60 + 100 * n, 200 + 100 * m)
 			var sprite = brick.get_child(0)
 			sprite.frame = rng.randi_range(0,9)
 			add_child(brick)
