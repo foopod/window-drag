d29912e6619365f185286d86c19388426c818d5f
Shung Fei <git@shungfei.com>
2023-07-24 09:13:12 +1200

    Use signals instead of Area2D for brick collision

--- scripts/Brick.gd
+++ scripts/Brick.gd
@@ -10,12 +10,7 @@ func _ready():
 	area = get_node('Area2D')
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var list = area.get_overlapping_bodies()
-	for body in list:
-		if body.name == "Ball":
-			await get_tree().create_timer(0.03).timeout
-			Global.iconData[row][column] = false
-			queue_free()
-	pass
+func _on_body_entered(body):
+	if body.name == "Ball":
+		Global.iconData[row][column] = false
+		queue_free()
