64b233b69bdba2e1be3855a9206adbdf5447ad20
Shung Fei <git@shungfei.com>
2023-07-25 22:05:48 +1200

    Clean up useless _ready() in brick.gd

--- scripts/Brick.gd
+++ scripts/Brick.gd
@@ -5,11 +5,6 @@ var area
 var row
 var column
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	area = get_node('Area2D')
-	pass # Replace with function body.
-
 func _on_body_entered(body):
 	if body.name == "Ball":
 		Global.iconData[row][column] = false
